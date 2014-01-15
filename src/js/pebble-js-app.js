e036a047e65a1220194810c9e36b71ce2a002677
Niraj Sanghvi <niraj.sanghvi@gmail.com>
2014-01-14 23:53:35 -0800

    Reduced JS slots to prevent error

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -6,20 +6,19 @@ Pebble.addEventListener("ready", function() {
 });
 
 Pebble.addEventListener("showConfiguration", function() {
-  var options = JSON.parse(window.localStorage.getItem("options"));
+  var saveOptions = JSON.parse(window.localStorage.getItem("options"));
 	
-  var url = "http://niraj.com/pebble/polar_clock_lite_config.php?v1.0";
+  var url = "http://niraj.com/pebble/polar_clock_lite_config.php?v=1.0";
 	
-  if(options != null) {
+  if(saveOptions != null) {
     url += "&time=" + (saveOptions["polarclockL0"] ? encodeURIComponent(saveOptions["polarclockL0"]) : "") + 
            "&date=" + (saveOptions["polarclockL1"] ? encodeURIComponent(saveOptions["polarclockL1"]) : "") +
 		   "&rowd=" + (saveOptions["polarclockL2"] ? encodeURIComponent(saveOptions["polarclockL2"]) : "") +
 		   "&invt=" + (saveOptions["polarclockL3"] ? encodeURIComponent(saveOptions["polarclockL3"]) : "") +
-		   "&four=" + (saveOptions["polarclockL4"] ? encodeURIComponent(saveOptions["polarclockL4"]) : "") +
-		   "&five=" + (saveOptions["polarclockL5"] ? encodeURIComponent(saveOptions["polarclockL5"]) : "") +
-		   "&six="  + (saveOptions["polarclockL6"] ? encodeURIComponent(saveOptions["polarclockL6"]) : "");
+		   "&four=" + (saveOptions["polarclockL4"] ? encodeURIComponent(saveOptions["polarclockL4"]) : "");
   }
-	console.log("showing configuration: " + url);
+	
+  console.log("Showing configuration: " + url);
   Pebble.openURL(url);
 });
 
@@ -32,14 +31,12 @@ Pebble.addEventListener("webviewclosed", function(e) {
 			console.log("Options = " + JSON.stringify(options));
 		
 			var saveOptions = {
-				"polarclockL0": options[0],
-				"polarclockL1": options[1],
-				"polarclockL2": options[2],
-				"polarclockL3": options[3],
-				"polarclockL4": options[4],
-				"polarclockL5": options[5],
-				"polarclockL6": options[6]
-			}
+				"polarclockL0": options["0"],
+				"polarclockL1": options["1"],
+				"polarclockL2": options["2"],
+				"polarclockL3": options["3"],
+				"polarclockL4": options["4"]
+			};
 		
 			window.localStorage.setItem("options", JSON.stringify(saveOptions));
 			
